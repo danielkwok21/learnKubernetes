eb3128c2e808ff53ef52800d8eb5348f2cf176c5
daniel kwok <epam.daniel-kwok@mercedes-benz.com>
2026-01-26 15:01:11 +0800

    learn operator - example

--- learnCRD/operator/api/v1alpha1/memcached_types.go
+++ learnCRD/operator/api/v1alpha1/memcached_types.go
@@ -25,14 +25,9 @@ import (
 
 // MemcachedSpec defines the desired state of Memcached
 type MemcachedSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-	// The following markers will use OpenAPI v3 schema to validate the value
-	// More info: https://book.kubebuilder.io/reference/markers/crd-validation.html
-
-	// foo is an example field of Memcached. Edit memcached_types.go to remove/update
-	// +optional
-	Foo *string `json:"foo,omitempty"`
+	// +kubebuilder:validation:Minimum=0
+	// +required
+	Size *int32 `json:"size,omitempty"`
 }
 
 // MemcachedStatus defines the observed state of Memcached.
